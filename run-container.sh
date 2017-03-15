435232341a187e2deab50999ca1d43ed1f304c31
Aron van Ammers <aron@blockstars.io>
2017-03-15 11:56:59 +0100

    Fix space

--- run-container.sh
+++ run-container.sh
@@ -4,7 +4,7 @@ source vars.sh
 
 BASEPATH=/home/aron/develop/public-data-works
 
-echo docker run --name $CONTAINER_NAME --net develop --ip $CONTAINER_IP_ADDRESS \
+docker run --name $CONTAINER_NAME --net develop --ip $CONTAINER_IP_ADDRESS \
 	-v $BASEPATH/home:/home/aron \
 	-v $BASEPATH/src:/develop \
 	-l ${MONGO_BIGCHAINDB_CONTAINER_NAME} -e MONGO_BIGCHAINDB_URL=mongodb://${MONGO_BIGCHAINDB_CONTAINER_NAME} \
