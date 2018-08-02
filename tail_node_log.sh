ac7cad26b376f745c4ad14c874dc95a25140da77
Aron van Ammers <aron@blockstars.io>
2018-08-02 13:30:38 +0200

    Show colors in log

--- tail_node_log.sh
+++ tail_node_log.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker logs -f iota_node_mainnet
+docker logs -f iota_node_mainnet | \
+	perl -pe 's/SOLID SUBTANGLE milestone/\e[1;32;40m$&\e[0m/g' | \
+	perl -pe 's/Latest milestone/\e[1;34;40m$&\e[0m/g'
+
